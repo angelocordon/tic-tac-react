9e44ba606c97b930842045f9e6d4ba4de0cc5cdb
Angelo Cordon <angelocordon@gmail.com>
2019-05-21 12:11:13 -0700

    Define Grid Size as state and property
    By default, define Grid Size as 3 (for 3x3 size grid) and pass it down as a
    prop to Board component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+
+import Board from './board';
 import './index.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // By default, grid size is 3, with ability to size up to N
+      gridSize: 3
+    }
+  }
+
   render () {
     return (
       <div className="columns">
-        <div className="column is-two-thirds"></div>
+        <div className="column is-two-thirds">
+          <Board gridSize={this.state.gridSize} />
+        </div>
         <div className="column is-one-third">
           <p>X, O</p>
           <button>Reset</button>
